ef66c87009eabb1f32d6966e4edb84f083eaac22
Sara980710 <sara@idala.eu>
2022-03-12 15:57:32 +0100

    example c++ code with CMake

---
+++ HelloWorld/src/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Helper/helper.h"
+
+int main() {
+    std::cout<<"Hello world!\n";
+    std::cout<<"Operation: 20.5 + 34.2 = "<< add(20.5f, 34.2f) << "\n";
+
+    return 0;
+}
